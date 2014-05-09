37de83ac1443cf62f9ae1fab984215ecb305fb77
fredtma <tshimanga@gmail.com>
2014-05-09 16:54:55 +0200

    2014-02-27 10:40 version 0.80.2

--- js/libs/angular/app.filters.js
+++ js/libs/angular/app.filters.js
@@ -1,4 +1,5 @@
-angular.module('KingsFilters', []).filter('ucfirst', function() {
+angular.module('KingsFilters', [])
+.filter('ucfirst', function() {
     return function(input) {
       return input.charAt(0).toUpperCase() + input.substring(1);
     }
@@ -10,4 +11,15 @@ angular.module('KingsFilters', []).filter('ucfirst', function() {
    return function(input){
       return input=timeDifference(input);
    }
-});
+})
+.filter('display',function(){
+   return function(input,option){
+      if(typeof option!=='object' || option==null)return input;
+      switch(option.case){
+         case 'pre': input="<b>"+option.value+"</b> "+input; break;
+         case 'suf': input+=" <b>"+option.value+"</b>"; break;
+      }
+      return input;
+   }
+})
+;
